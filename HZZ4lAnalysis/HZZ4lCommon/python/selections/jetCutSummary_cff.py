916ae3f7a54a0bef2300e146bfaf41ab09f3139f
scasasso <>
2012-03-01 10:36:08 +0000

    fix wrong import

--- HZZ4lAnalysis/HZZ4lCommon/python/selections/jetCutSummary_cff.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/selections/jetCutSummary_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from HZZ4lAnalysis.HZZ4lCommon.selections.cutSummaryPFJet_cfi import *
+from CMGTools.Common.selections.cutSummaryPFJet_cfi import *
 
 jetCutSummarySequence = cms.Sequence(
     cutSummaryPFJet
