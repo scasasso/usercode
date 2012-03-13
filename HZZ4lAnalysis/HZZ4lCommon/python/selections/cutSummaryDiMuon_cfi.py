6a23b0eca78523489c021e56ef7033d9f9cb1147
scasasso <>
2012-03-13 16:11:49 +0000

    *** empty log message ***

--- HZZ4lAnalysis/HZZ4lCommon/python/selections/cutSummaryDiMuon_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-cutSummaryDiMuon = cms.EDAnalyzer(
-    "CutSummaryAnalyzer",
-    inputCollection = cms.InputTag("cmgDiMuonSel")
-)
--- HZZ4lAnalysis/HZZ4lCommon/python/selections/muonCutSummary_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HZZ4lAnalysis.HZZ4lCommon.selections.cutSummaryMuon_cfi import *
-from HZZ4lAnalysis.HZZ4lCommon.selections.cutSummaryDiMuon_cfi import *
-
-muonCutSummarySequence = cms.Sequence(
-    cutSummaryMuon +
-    cutSummaryDiMuon
-    )
