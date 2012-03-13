ca4913f9ef124e597aee418a4f70986269282b1b
scasasso <>
2012-03-13 16:56:54 +0000

    *** empty log message ***

--- HZZ4lAnalysis/HZZ4lCommon/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/HZZ4lAnalysis/HZZ4lCommon/',1)[0])+'/cfipython/slc5_amd64_gcc434/HZZ4lAnalysis/HZZ4lCommon')
