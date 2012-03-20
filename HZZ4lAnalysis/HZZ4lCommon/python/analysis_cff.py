6ae0181100d0cff371c84e41d9e5a3dd44195acb
scasasso <>
2012-03-20 14:40:23 +0000

    several new sequences added

--- HZZ4lAnalysis/HZZ4lCommon/python/analysis_cff.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/analysis_cff.py
@@ -4,20 +4,29 @@ import FWCore.ParameterSet.Config as cms
 from CMGTools.Common.jet_cff import *
 from CMGTools.Common.met_cff import *
 from HZZ4lAnalysis.HZZ4lCommon.muon_cff import *
-from HZZ4lAnalysis.HZZ4lCommon.diLepton_cff import *
 from HZZ4lAnalysis.HZZ4lCommon.electron_cff import *
+from HZZ4lAnalysis.HZZ4lCommon.diLepton_cff import *
+from HZZ4lAnalysis.HZZ4lCommon.triLepton_cff import *
+from HZZ4lAnalysis.HZZ4lCommon.quadriLepton_cff import *
 from CMGTools.Common.tau_cff import *
 from HZZ4lAnalysis.HZZ4lCommon.higgs_cff import *
 from HZZ4lAnalysis.HZZ4lCommon.cutSummary_cff import *
 from CMGTools.Common.histogram_cff import *
 from CMGTools.Common.trigger_cff import *
+from HZZ4lAnalysis.HZZ4lCommon.cmgCandMerge_cfi import *
 
 cmgObjectSequence = cms.Sequence(
     jetSequence +
     metSequence +
     muonSequence +
     electronSequence +
+    leptonMergingSequence +
     diLeptonSequence +
+    dileptonMergingSequence +
+    triLeptonSequence +
+    trileptonMergingSequence +
+    quadriLeptonSequence +
+    quadrileptonMergingSequence +
     tauSequence +
     higgsSequence +
     triggerSequence
@@ -27,6 +36,8 @@ cmgObjectSequence = cms.Sequence(
 analysisSequence = cms.Sequence(
     # build the CMG objects and select them
     cmgObjectSequence +
+    # build miscellaneous objects not inheriting from cmg::PhysicsObject
+    #miscSequence + 
     # summary of cuts for all objects, after skimming
     cutSummarySequence 
     # histograms for all objects, after skimming
