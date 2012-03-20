1429ac35e5a79048ebafaebde1e8d628566ecfa8
scasasso <>
2012-03-20 14:41:52 +0000

    re-organizing the sequences

--- HZZ4lAnalysis/HZZ4lCommon/python/higgs_cff.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/higgs_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from HZZ4lAnalysis.HZZ4mu.higgs4mu_cff import *
-from HZZ4lAnalysis.HZZ4e.higgs4e_cff import *
-from HZZ4lAnalysis.HZZ2e2mu.higgs2e2mu_cff import *
+from HZZ4lAnalysis.HZZ4mu.higgs4mu_cff import higgs4muSequence
+from HZZ4lAnalysis.HZZ4e.higgs4e_cff import higgs4eSequence
+from HZZ4lAnalysis.HZZ2e2mu.higgs2e2mu_cff import higgs2e2muSequence
 
 higgsSequence = (
     higgs4muSequence +
