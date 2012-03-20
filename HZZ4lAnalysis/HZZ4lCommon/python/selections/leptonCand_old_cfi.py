89e26685f671ed16ea551314b7663ff3156a2963
scasasso <>
2012-03-20 15:25:03 +0000

    soft lepton selection added

--- HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
@@ -30,9 +30,20 @@ electronCand = cms.PSet(
 #Muon Candidates
 muonCand = cms.PSet(
     isGlobal = cms.string('isGlobal()'),
-    numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the safest choice to ask the muon to be Global?
+    numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
     sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 100.'),
     looseiso = cms.string('sourcePtr.userFloat("RhoCorrIso") < 0.7'),
     pt = cms.string('pt() > 5'),
     eta = cms.string('abs(eta()) < 2.4')
+    )
+
+#Loose muon candidates
+muonCandLoose = cms.PSet(
+    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
+    pt = cms.string('pt() > 5')
+    )
+
+#Lose electron candidates
+electronCandLoose = cms.PSet(
+    pt = cms.string('pt() > 5')
     )
