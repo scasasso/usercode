936b7b0ed4fd4a37008ee8a82cf98811d80a6707
scasasso <>
2012-03-20 14:37:20 +0000

    new PO producers

--- HZZ4lAnalysis/HZZ4lCommon/plugins/CMGTools.cc
+++ HZZ4lAnalysis/HZZ4lCommon/plugins/CMGTools.cc
@@ -6,14 +6,24 @@
 
 DEFINE_FWK_MODULE(DiGenParticlePOProducer);
 
+//DiObject producers
 DEFINE_FWK_MODULE(EMuPOProducer);
 
+//TriObject producers
+DEFINE_FWK_MODULE(DiMuonMuPOProducer);
+DEFINE_FWK_MODULE(DiMuonEPOProducer);
+DEFINE_FWK_MODULE(DiElectronEPOProducer);
+DEFINE_FWK_MODULE(DiElectronMuPOProducer);
+
+//QuadriObject producers
 DEFINE_FWK_MODULE(DiMuonDiMuonPOProducer);
 DEFINE_FWK_MODULE(DiElectronDiElectronPOProducer);
 DEFINE_FWK_MODULE(DiElectronDiMuonPOProducer);
-
+DEFINE_FWK_MODULE(DiMuonEMuPOProducer);
+DEFINE_FWK_MODULE(DiElectronEMuPOProducer);
 DEFINE_FWK_MODULE(DiGenParticleDiGenParticlePOProducer);
 
+//Higgs Candidate producers
 //DEFINE_FWK_MODULE(DiGenParticleDiGenParticleHiggsPOProducer);
 DEFINE_FWK_MODULE(DiMuonDiMuonHiggsPOProducer);
 DEFINE_FWK_MODULE(DiElectronDiElectronHiggsPOProducer);
--- HZZ4lAnalysis/HZZ4lCommon/plugins/CMGTools.h
+++ HZZ4lAnalysis/HZZ4lCommon/plugins/CMGTools.h
@@ -8,15 +8,25 @@
 
 typedef PhysicsObjectProducer<cmg::DiGenParticleFactory> DiGenParticlePOProducer;
 
+//DiObject producers
 typedef PhysicsObjectProducer<cmg::EMuFactory> EMuPOProducer;
 
+//TriObject producers
+typedef PhysicsObjectProducer<cmg::DiMuonMuFactory> DiMuonMuPOProducer;
+typedef PhysicsObjectProducer<cmg::DiMuonEFactory> DiMuonEPOProducer;
+typedef PhysicsObjectProducer<cmg::DiElectronEFactory> DiElectronEPOProducer;
+typedef PhysicsObjectProducer<cmg::DiElectronMuFactory> DiElectronMuPOProducer;
+
+//QuadriObject producers
 typedef PhysicsObjectProducer<cmg::DiMuonDiMuonFactory> DiMuonDiMuonPOProducer;
 typedef PhysicsObjectProducer<cmg::DiElectronDiElectronFactory> DiElectronDiElectronPOProducer;
 typedef PhysicsObjectProducer<cmg::DiElectronDiMuonFactory> DiElectronDiMuonPOProducer;
+typedef PhysicsObjectProducer<cmg::DiMuonEMuFactory> DiMuonEMuPOProducer;
+typedef PhysicsObjectProducer<cmg::DiElectronEMuFactory> DiElectronEMuPOProducer;
 typedef PhysicsObjectProducer<cmg::DiGenParticleDiGenParticleFactory> DiGenParticleDiGenParticlePOProducer;
 
+//Higgs Candidate producers
 //typedef PhysicsObjectProducer<cmg::DiGenParticleDiGenParticleHiggsFactory> DiGenParticleDiGenParticleHiggsPOProducer;
-
 typedef PhysicsObjectProducer<cmg::DiMuonDiMuonHiggsFactory> DiMuonDiMuonHiggsPOProducer;
 typedef PhysicsObjectProducer<cmg::DiElectronDiElectronHiggsFactory> DiElectronDiElectronHiggsPOProducer;
 typedef PhysicsObjectProducer<cmg::DiElectronDiMuonHiggsFactory> DiElectronDiMuonHiggsPOProducer;
