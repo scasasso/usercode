0fd948439d45f6890638a16d7e3c7d9b05617412
scasasso <>
2012-03-20 14:33:45 +0000

    new typedefs

--- HZZ4lAnalysis/HZZ4lCommon/interface/CompoundFactories.h
+++ HZZ4lAnalysis/HZZ4lCommon/interface/CompoundFactories.h
@@ -9,15 +9,25 @@
 namespace cmg{
   typedef DiObjectFactory< cmg::DiGenParticle::type1, cmg::DiGenParticle::type2 > DiGenParticleFactory;
 
+  //DiObject factories
   typedef DiObjectFactory< cmg::EMu::type1, cmg::EMu::type2 > EMuFactory;
-  
+
+  //TriObject factories
+  typedef DiObjectFactory< cmg::DiMuonMu::type1, cmg::DiMuonMu::type2 > DiMuonMuFactory;
+  typedef DiObjectFactory< cmg::DiMuonE::type1, cmg::DiMuonE::type2 > DiMuonEFactory;
+  typedef DiObjectFactory< cmg::DiElectronE::type1, cmg::DiElectronE::type2 > DiElectronEFactory;
+  typedef DiObjectFactory< cmg::DiElectronMu::type1, cmg::DiElectronMu::type2 > DiElectronMuFactory;
+
+  //QuadriObject factories  
   typedef DiObjectFactory< cmg::DiMuonDiMuon::type1, cmg::DiMuonDiMuon::type2 > DiMuonDiMuonFactory;
   typedef DiObjectFactory< cmg::DiElectronDiElectron::type1, cmg::DiElectronDiElectron::type2 > DiElectronDiElectronFactory;
   typedef DiObjectFactory< cmg::DiElectronDiMuon::type1, cmg::DiElectronDiMuon::type2 > DiElectronDiMuonFactory;
+  typedef DiObjectFactory< cmg::DiMuonEMu::type1, cmg::DiMuonEMu::type2 > DiMuonEMuFactory;
+  typedef DiObjectFactory< cmg::DiElectronEMu::type1, cmg::DiElectronEMu::type2 > DiElectronEMuFactory;
   typedef DiObjectFactory< cmg::DiGenParticleDiGenParticle::type1, cmg::DiGenParticleDiGenParticle::type2 > DiGenParticleDiGenParticleFactory;
 
+  //Higgs Candidate factories
   //typedef HiggsCandidateFactory< cmg::DiGenParticleDiGenParticle::type1, cmg::DiGenParticleDiGenParticle::type2 > DiGenParticleDiGenParticleHiggsFactory;
-
   typedef HiggsCandidateFactory< cmg::DiElectronDiMuon::type1, cmg::DiElectronDiMuon::type2 > DiElectronDiMuonHiggsFactory;
   typedef HiggsCandidateFactory< cmg::DiMuonDiMuon::type1, cmg::DiMuonDiMuon::type2 > DiMuonDiMuonHiggsFactory;
   typedef HiggsCandidateFactory< cmg::DiElectronDiElectron::type1, cmg::DiElectronDiElectron::type2 > DiElectronDiElectronHiggsFactory;
