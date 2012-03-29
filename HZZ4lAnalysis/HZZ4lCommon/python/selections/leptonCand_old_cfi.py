bb7ecb8fe1f1abd05e5317b59283970d5e9c923a
scasasso <>
2012-03-29 21:01:12 +0000

    tight selections added

--- HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
@@ -1,8 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#Electron candidates
-
-#Useful functions
 
 #electron identification
 def addCiCIDSelection(var):
@@ -17,11 +14,23 @@ cicMediumID     = addCiCIDSelection('eidMedium')
 cicTightID      = addCiCIDSelection('eidTight')
 cicSuperTightID = addCiCIDSelection('eidSuperTight')
 
+#Loose muon candidates (soft muons)
+muonCandLoose = cms.PSet(
+    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
+    pt = cms.string('pt() > 5')
+    )
+
+#Loose electron candidates (soft electrons)
+electronCandLoose = cms.PSet(
+    pt = cms.string('pt() > 5')
+    )
+
+
 #Electron Candidates
 electronCand = cms.PSet(
     cicTight = cicTightID.clone(), #waiting to pass to MVA
     numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1'),
-    looseiso = cms.string('sourcePtr.userFloat("RhoCorrIso") < 0.7'),
+    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
     sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 100.'),
     pt = cms.string('pt() > 7'),
     eta = cms.string('abs(eta()) < 2.5')
@@ -32,18 +41,27 @@ muonCand = cms.PSet(
     isGlobal = cms.string('isGlobal()'),
     numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
     sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 100.'),
-    looseiso = cms.string('sourcePtr.userFloat("RhoCorrIso") < 0.7'),
+    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
     pt = cms.string('pt() > 5'),
     eta = cms.string('abs(eta()) < 2.4')
     )
 
-#Loose muon candidates
-muonCandLoose = cms.PSet(
-    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
-    pt = cms.string('pt() > 5')
+#Tight Electron Candidates (Z1)
+electronCandTight = cms.PSet(
+    cicTight = cicTightID.clone(), #waiting to pass to MVA
+    numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1'),
+    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
+    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 4.'),
+    pt = cms.string('pt() > 10'),
+    eta = cms.string('abs(eta()) < 2.5')
     )
 
-#Lose electron candidates
-electronCandLoose = cms.PSet(
-    pt = cms.string('pt() > 5')
+#Tight Muon Candidates (Z1)
+muonCandTight = cms.PSet(
+    isGlobal = cms.string('isGlobal()'),
+    numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
+    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 4.'),
+    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
+    pt = cms.string('pt() > 10'),
+    eta = cms.string('abs(eta()) < 2.4')
     )
