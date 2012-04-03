ff13acfd5d0b7c90d9efe4e59849f6ed897d5e23
scasasso <>
2012-04-03 00:17:04 +0000

    stupid bug, maybe too late to work? :)

--- HZZ4lAnalysis/HZZ4lCommon/prod/cmgTuple_cfg.py
+++ HZZ4lAnalysis/HZZ4lCommon/prod/cmgTuple_cfg.py
@@ -30,7 +30,7 @@ options.register('maxEvents',-1,VarParsing.VarParsing.multiplicity.singleton,Var
 options.parseArguments()
 
 
-Pick up the data files (ggHZZTo4l).
+#Pick up the data files (ggHZZTo4l).
 process.source = datasetToSource(
     "cmgtools",
     "/GluGluToHToZZTo4L_M-120_7TeV-powheg-pythia6/Fall11-PU_S6_START42_V14B-v1/AODSIM/V4/PAT_CMG_4_0_0",
