5a15b176e1c94dba8d36d9c9ff47742596f9468f
scasasso <>
2012-04-22 21:19:29 +0000

    *** empty log message ***

--- HZZ4lAnalysis/HZZ4lCommon/python/selections/diLepton_cfi.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/selections/diLepton_cfi.py
@@ -1,45 +1,93 @@
 import FWCore.ParameterSet.Config as cms
 from leptonCand_old_cfi import *
 
+###########
+### Z1 cuts
+###########
+
 #Z1mumu cuts
 z1mumu = cms.PSet(
     os = cms.string('leg1().charge()*leg2().charge()<0'),
     mass = cms.string('mass() >= 50 && mass() < 120'),             
-    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandTight\")'),# to be implemented
-    leg1_revisited = cms.PSet(
-    pt = cms.string('leg1().pt() > 20')
-    ),
-    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandTight\")'),# to be implemented
-    leg2_revisited = cms.PSet(
-    pt = cms.string('leg2().pt() > 10')
-    ),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandTight\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandTight\")'),
+    ptleptons = cms.string('( leg1.pt() > 20. && leg2.pt() > 10. ) || ( leg2.pt() > 20. && leg1.pt() > 10. )'),
     isolation = cms.string('leg1().sourcePtr().userFloat("RhoCorrIso") + leg2().sourcePtr().userFloat("RhoCorrIso") < 0.35') #Rho correct isolation
     )
 
 #Z1ee cuts
 z1ee = cms.PSet(
     os = cms.string('leg1().charge()*leg2().charge()<0'),
-    mass = cms.string('mass() >= 50 && mass() < 120'),             
-    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandTight\")'),# to be implemented
-    leg1_revisited = cms.PSet(
-    pt = cms.string('leg1().pt() > 20')
-    ),
-    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandTight\")'),# to be implemented
-    leg2_revisited = cms.PSet(
-    pt = cms.string('leg2().pt() > 10')
-    ),
+    mass = cms.string('mass() >= 50 && mass() < 120'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandTight\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandTight\")'),
+    ptleptons = cms.string('( leg1.pt() > 20. && leg2.pt() > 10. ) || ( leg2.pt() > 20. && leg1.pt() > 10. )'),
     isolation = cms.string('leg1().sourcePtr().userFloat("RhoCorrIso") + leg2().sourcePtr().userFloat("RhoCorrIso") < 0.35') #Rho correct isolation
     )
 
-#llCand (EMu, diLeps)
-llCand = cms.PSet(
-    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandLoose\")'),
-    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandLoose\")')
+###########
+### Z2 cuts
+###########
+
+#Z2mumu cuts
+z2mumu = cms.PSet(
+    os = cms.string('leg1().charge()*leg2().charge()<0'),
+    mass = cms.string('mass() >= 12 && mass() < 120'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCand\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCand\")')
     )
 
-#zCand (zMM, zEE, zLL)
-zCand = cms.PSet(
+#Z2ee cuts
+z2ee = cms.PSet(
     os = cms.string('leg1().charge()*leg2().charge()<0'),
-    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandLoose\")'),
-    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandLoose\")')
+    mass = cms.string('mass() >= 12 && mass() < 120'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCand\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCand\")')
+    )
+
+#######################################
+### Dilepton cuts before best Z1 choice
+#######################################
+
+zCandplusID = cms.PSet(
+    os = cms.string('leg1().charge()*leg2().charge()<0'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandplusID\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandplusID\")')
+    )
+
+###################################################
+### LL cuts for extended phase space control region
+###################################################
+llCandplusID = cms.PSet(
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandplusID\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandplusID\")')
+    )
+
+
+##################################
+### LL cuts for Z+X control region
+##################################
+
+#OS-SF
+llCandplusSIPOSSF = cms.PSet(
+    os = cms.string('leg1().charge()*leg2().charge()<0'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandplusSIP\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandplusSIP\")')
+    )
+
+#SS-SF
+llCandplusSIPSSSF = cms.PSet(
+    ss = cms.string('leg1().charge()*leg2().charge()>0'),
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandplusSIP\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandplusSIP\")')
+    )
+
+
+
+##########################################
+### LL cuts for Zbb and tt control regions
+##########################################
+llCandInvertedSIPplusID = cms.PSet(
+    leg1 = cms.string('leg1().getSelection(\"cuts_leptonCandInvertedSIPplusID\")'),
+    leg2 = cms.string('leg2().getSelection(\"cuts_leptonCandInvertedSIPplusID\")')
     )
--- HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
+++ HZZ4lAnalysis/HZZ4lCommon/python/selections/leptonCand_old_cfi.py
@@ -14,6 +14,10 @@ cicMediumID     = addCiCIDSelection('eidMedium')
 cicTightID      = addCiCIDSelection('eidTight')
 cicSuperTightID = addCiCIDSelection('eidSuperTight')
 
+###########################################
+### Lepton  candidates for LLLL collections
+###########################################
+
 #Loose muon candidates (soft muons)
 muonCandLoose = cms.PSet(
     isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
@@ -26,32 +30,95 @@ electronCandLoose = cms.PSet(
     )
 
 
-#Electron Candidates
+########################################################
+### Lepton candidates for extended phase space selection
+########################################################
+
+#Adding ID
+muonCandplusID = cms.PSet(
+    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
+    pt = cms.string('pt() > 5'),
+    isGlobal = cms.string('isGlobal()'),
+    numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10') #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
+    )
+
+electronCandplusID = cms.PSet(
+    pt = cms.string('pt() > 5'),
+    cicTight = cicTightID.clone(), #waiting to pass to MVA
+    numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1')
+    )
+
+
+
+##############################################################
+### Lepton candidates for Zbb and tt control regions selection
+##############################################################
+
+#Inverted SIP and ID
+muonCandInvertedSIPplusID = cms.PSet(
+    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
+    pt = cms.string('pt() > 5'),
+    isGlobal = cms.string('isGlobal()'),
+    numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
+    sip = cms.string('sourcePtr().userFloat("SIP3D") > 5.')
+    )
+
+electronCandInvertedSIPplusID = cms.PSet(
+    pt = cms.string('pt() > 5'),
+    cicTight = cicTightID.clone(), #waiting to pass to MVA
+    numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1'),
+    sip = cms.string('sourcePtr().userFloat("SIP3D") > 5.')
+    )
+
+#######################################################
+### Lepton candidates for Z+X control regions selection
+#######################################################
+
+muonCandplusSIP = cms.PSet(
+    isGlobalOrTracker = cms.string('isGlobal() || isTracker()'),
+    pt = cms.string('pt() > 5'),
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.')
+    )
+
+electronCandplusSIP = cms.PSet(
+    pt = cms.string('pt() > 5'),
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.')
+    )
+
+######################################
+### Lepton candidates for Z2 selection
+######################################
+
+#Electron Candidates (Z2)
 electronCand = cms.PSet(
     cicTight = cicTightID.clone(), #waiting to pass to MVA
     numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1'),
-    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
-    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 100.'),
+    looseiso = cms.string('sourcePtr().trackIso()/pt() < 0.7'), #Remember to switch to UserIso ASAP
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.'),
     pt = cms.string('pt() > 7'),
     eta = cms.string('abs(eta()) < 2.5')
     )
 
-#Muon Candidates
+#Muon Candidates (Z2)
 muonCand = cms.PSet(
     isGlobal = cms.string('isGlobal()'),
     numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
-    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 100.'),
-    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.'),
+    looseiso = cms.string('sourcePtr().trackIso()/pt() < 0.7'), #Remember to switch to UserIso ASAP
     pt = cms.string('pt() > 5'),
     eta = cms.string('abs(eta()) < 2.4')
     )
+
+######################################
+### Lepton candidates for Z1 selection
+######################################
 
 #Tight Electron Candidates (Z1)
 electronCandTight = cms.PSet(
     cicTight = cicTightID.clone(), #waiting to pass to MVA
     numberOfHits = cms.string('sourcePtr().gsfTrack().trackerExpectedHitsInner().numberOfHits()<=1'),
-    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
-    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 4.'),
+    looseiso = cms.string('sourcePtr().trackIso()/pt() < 0.7'), #Remember to switch to UserIso ASAP
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.'),
     pt = cms.string('pt() > 10'),
     eta = cms.string('abs(eta()) < 2.5')
     )
@@ -60,8 +127,8 @@ electronCandTight = cms.PSet(
 muonCandTight = cms.PSet(
     isGlobal = cms.string('isGlobal()'),
     numberOfValidHits = cms.string('isGlobal() && sourcePtr().numberOfValidHits() > 10'), #need to get rid of the complaint of undefined ref to reco::Track. It's really the most safe choice to ask the muon to be Global?
-    sip = cms.string('sourcePtr().dB()/sourcePtr().edB() < 4.'),
-    looseiso = cms.string('sourcePtr().trackIso() < 0.7'), #Remember to switch to UserIso ASAP
+    sip = cms.string('sourcePtr().userFloat("SIP3D") < 4.'),
+    looseiso = cms.string('sourcePtr().trackIso()/pt() < 0.7'), #Remember to switch to UserIso ASAP
     pt = cms.string('pt() > 10'),
     eta = cms.string('abs(eta()) < 2.4')
     )
