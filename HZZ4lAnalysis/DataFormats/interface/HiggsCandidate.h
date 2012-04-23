664d68664447dbd40087d76015a919eb68f78284
sbologne <>
2012-04-23 09:14:01 +0000

    move addUserFloat and similia into DiOject.h, needed for Z too

--- HZZ4lAnalysis/DataFormats/interface/HiggsCandidate.h
+++ HZZ4lAnalysis/DataFormats/interface/HiggsCandidate.h
@@ -21,11 +21,11 @@ namespace cmg {
       helcosthetaZ1_(UnSet(Double_t)),
       helcosthetaZ2_(UnSet(Double_t)),
       phistarZ1_(UnSet(Double_t)),
-      phistarZ2_(UnSet(Double_t)),
-      userFloatLabels_(),
-      userFloats_(),
-      userBoolLabels_(),
-      userBools_(){
+      phistarZ2_(UnSet(Double_t)) {//,
+      //userFloatLabels_(),
+      //userFloats_(),
+      //userBoolLabels_(),
+      //userBools_(){
     }
     HiggsCandidate(const HiggsCandidate<TL1,TL2>& other):
       DiObject<TL1,TL2>(other),
@@ -36,11 +36,11 @@ namespace cmg {
       helcosthetaZ1_(other.helcosthetaZ1_),
       helcosthetaZ2_(other.helcosthetaZ2_),
       phistarZ1_(other.phistarZ1_),
-      phistarZ2_(other.phistarZ2_),
-      userFloatLabels_(other.userFloatLabels_),
-      userFloats_(other.userFloats_),
-      userBoolLabels_(other.userBoolLabels_),
-      userBools_(other.userBools_){
+      phistarZ2_(other.phistarZ2_) {//,
+      //userFloatLabels_(other.userFloatLabels_),
+      //userFloats_(other.userFloats_),
+      //userBoolLabels_(other.userBoolLabels_),
+	//userBools_(other.userBools_){
       }
       
     HiggsCandidate(const DiObject<TL1,TL2>& other):
@@ -52,11 +52,11 @@ namespace cmg {
       helcosthetaZ1_(UnSet(Double_t)),
       helcosthetaZ2_(UnSet(Double_t)),
       phistarZ1_(UnSet(Double_t)),
-      phistarZ2_(UnSet(Double_t)),
-      userFloatLabels_(),
-      userFloats_(),
-      userBoolLabels_(),
-      userBools_(){
+      phistarZ2_(UnSet(Double_t)) {//,
+		  //userFloatLabels_(),
+		  //userFloats_(),
+		  //userBoolLabels_(),
+		  //userBools_(){
     }
 
     virtual ~HiggsCandidate(){}
@@ -70,24 +70,24 @@ namespace cmg {
     Double_t phistarZl1() const{ return phistarZ1_;}
     Double_t phistarZl2() const{ return phistarZ2_;}
 
-    float userFloat( const std::string & key ) const;
+    /*float userFloat( const std::string & key ) const;
     float userFloat( const char* key ) const {return userFloat(std::string(key));};
     void addUserFloat( const  std::string & label, float data );
     const std::vector<std::string> & userFloatNames() const  { return userFloatLabels_; }
     bool hasUserFloat( const char * key ) const { return hasUserFloat(std::string(key)); }
     bool hasUserFloat( const std::string & key ) const {
       return std::find(userFloatLabels_.begin(), userFloatLabels_.end(), key) != userFloatLabels_.end();
-    }
+      }*/
 
     
-    bool userBool( const std::string & key ) const;
+    /*bool userBool( const std::string & key ) const;
     bool userBool( const char* key ) const {return userBool(std::string(key));};
     void addUserBool( const  std::string & label, bool data );
     const std::vector<std::string> & userBoolNames() const  { return userBoolLabels_; }
     bool hasUserBool( const char * key ) const { return hasUserBool(std::string(key)); }
     bool hasUserBool( const std::string & key ) const {
       return std::find(userBoolLabels_.begin(), userBoolLabels_.end(), key) != userBoolLabels_.end();
-    }
+      }*/
     
   
   private:
@@ -100,16 +100,16 @@ namespace cmg {
     Double_t phistarZ1_;
     Double_t phistarZ2_;
 
-    std::vector<std::string>      userFloatLabels_;
+    /*std::vector<std::string>      userFloatLabels_;
     std::vector<float>            userFloats_;
 
     std::vector<std::string>      userBoolLabels_;
-    std::vector<bool>            userBools_;
+    std::vector<bool>            userBools_;*/
 
     friend class cmg::HiggsCandidateFactory<TL1,TL2>;
     
   };
-
+  /*
   template< typename TL1, typename TL2 >
   float HiggsCandidate<TL1,TL2>::userFloat( const std::string &key ) const
   {
@@ -144,7 +144,7 @@ namespace cmg {
     userBoolLabels_.push_back(label);
     userBools_.push_back( data );
   }
-  
+  */
 }
 
 #endif
