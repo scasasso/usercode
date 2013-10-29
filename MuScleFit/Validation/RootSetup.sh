d4abc1aa0759f883277a7f6fcf95555b7ddd6adc
Ernesto Migliore <migliore@to.infn.it>
2013-10-29 13:12:12 +0100

    updates for CMSSW_5_3_9

--- MuScleFit/Validation/RootSetup.sh
+++ MuScleFit/Validation/RootSetup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
 
 
-source /afs/cern.ch/sw/lcg/external/gcc/4.7.2/x86_64-slc6-gcc47-opt/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.07/x86_64-slc6-gcc46-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/external/gcc/4.6.2/x86_64-slc5-gcc46-opt/setup.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.32.00/x86_64-slc5-gcc46-opt/root/bin/thisroot.sh
 
-export PATH=$PATH:/afs/cern.ch/sw/lcg/external/xrootd/3.1.0p2/x86_64-slc6-gcc47-opt/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/sw/lcg/external/xrootd/3.1.0p2/x86_64-slc6-gcc47-opt/lib
+export PATH=$PATH:/afs/cern.ch/sw/lcg/external/xrootd/3.1.0p2/x86_64-slc5-gcc46-opt/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/sw/lcg/external/xrootd/3.1.0p2/x86_64-slc5-gcc46-opt/lib
 
 touch foo.C
 echo '{std::cout<<"Congratulations: you have set up ROOT and Roofit!"<<std::endl;} ' >> foo.C
